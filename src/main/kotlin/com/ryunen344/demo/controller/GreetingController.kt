6cf732f6d9012dbcba406ef0a45bdc74746dd8c1
RyuNen344 <s1100633@outlook.com>
2018-12-23 23:16:33 +0900

    gradel周りを手入れ

--- src/main/kotlin/com/ryunen344/demo/controller/GreetingController.kt
+++ src/main/kotlin/com/ryunen344/demo/controller/GreetingController.kt
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 
+
 @Controller
 @RequestMapping("greeting")
 class GreetingController {
@@ -16,23 +17,5 @@ class GreetingController {
             model: Model): String {
         model.addAttribute("name", name)
         return "greeting"
-    }
-}
-
-class Customer(name: String) {
-    // 初期化処理はinitブロックの中に書く
-    init {
-        // プライマリコンストラクタの引数がこの中で使える
-        println("Customer initialized with value ${name}")
-    }
-
-    // プライマリコンストラクタの引数がプロパティの初期化でも使える
-    val customerKey = name.toUpperCase()
-
-    // セカンダリコンストラクタ
-    constructor(firstName: String, lastName: String)
-    // プライマリがある場合、セカンダリは必ずプライマリを呼び出す
-            : this("$firstName $lastName") {
-        println("firstName = $firstName, lastName = $lastName")
     }
 }
