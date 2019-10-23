dc3a51a4e50a5d6d33cbe3901947abc978a0e5f6
Bunjiro Miyoshi <s1100633@outlook.com>
2019-10-24 01:02:08 +0900

    webflux configurerの継承クラスを追加

---
+++ src/main/kotlin/com/ryunen344/demo/config/WebConfig.kt
@@ -0,0 +1,8 @@
+package com.ryunen344.demo.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class WebConfig : WebFluxConfigurer {
+}
